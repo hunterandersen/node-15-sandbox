fa5f263def362cb29f723a0613e187425452152c
Hunter Andersen <andersen.hunter42@gmail.com>
2023-07-06 07:26:26 -0700

    Adding Ronnies Sandbox

---
+++ ronnieScript.js
@@ -0,0 +1,7 @@
+// Selecting elements
+const sloganElement = document.getElementById("slogan");
+console.log(sloganElement);
+
+sloganElement.innerText = "Best Waffles This Side of the Appalachian!";
+
+sloganElement.style.fontSize = "28px";
