bbb9632bdf5c433b5b7e7a381fd89ab11d6228b0
Hunter Andersen <andersen.hunter42@gmail.com>
2023-07-08 18:18:20 -0700

    Refactored functionality for adding items to cart

--- practice.js
+++ practice.js
@@ -1,6 +1,3 @@
-const addToCartButtons = document.querySelectorAll(".add-to-cart");
-
-
 /**
  * Describes one item that a customer can purchase
  */
@@ -107,6 +104,7 @@ function displayShoppingItems(){
         price.classList.add("shopping-item-price");
         addBtn.textContent = `Add to Cart`;
         addBtn.classList.add("shopping-item-addToCart");
+        addBtn.addEventListener("click", () => addItemToCart(item));
         //Append elements
         priceBtnDiv.append(price, addBtn);
         itemCard.append(title, description, priceBtnDiv);
@@ -115,46 +113,41 @@ function displayShoppingItems(){
     })
 }
 
-const tallySheet = {};
+/**
+ * 
+ * @param {ShoppingItem} item 
+ */
+function addItemToCart(item){
+    //I need to know which button was clicked
+    //and what item that refers back to
+    console.log(item);
+    //Track that that specific item was clicked again
+    item.addToCart();
+    //Display the up-to-date cart
+    displayCart();
+}
 
-addToCartButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        //Update a tally sheet of shopping cart items
-            //I need to know which button was clicked
-            //and what item that refers back to
-            const parent = event.target.parentElement;
-            const itemName = parent.firstElementChild.textContent;
-            console.log(itemName);
-            //Track that that specific item was clicked again
-            if (!tallySheet[itemName]){
-                tallySheet[itemName] = 1;
-            } else {
-                tallySheet[itemName]++;
-            }
-        //Display the up-to-date version
-        displayCart();
-    });
-});
+const tallySheet = {};
 
 function displayCart(){
     //Clear out the old cart
     const cartList = document.querySelector(".shopping-cart > ul");
     cartList.innerHTML = null;
     //Create the shopping cart items one by one
-    const tallyKeys = Object.keys(tallySheet);
-    for (let i = 0; i < tallyKeys.length; i++){
-        const newLi = document.createElement("li");
-        const p1 = document.createElement("p");
-        const p2 = document.createElement("p");
-
-        p1.textContent = `${tallyKeys[i]}`;
-        p2.textContent = `x${tallySheet[tallyKeys[i]]}`;
-
-        //Add the elements to the page
-        newLi.append(p1, p2);
-        cartList.appendChild(newLi);
-    }
-    //Add them to the DOM (aka display them)
+    mockItemData.forEach(item => {
+        if (item.quantity){
+            //Create elements
+            const newLi = document.createElement("li");
+            const p1 = document.createElement("p");
+            const p2 = document.createElement("p");
+            //Modify Elements
+            p1.textContent = `${item.title}`;
+            p2.textContent = `x${item.quantity}`;
+            //Append the Elements
+            newLi.append(p1, p2);
+            cartList.appendChild(newLi);
+        }
+    });
 }
 
 displayShoppingItems();
