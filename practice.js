5f79a55d13f6f586c2dbd9350b03f38b111c1d2f
Hunter Andersen <andersen.hunter42@gmail.com>
2023-07-08 18:10:55 -0700

    Generates cards, added card styling

--- practice.js
+++ practice.js
@@ -83,6 +83,38 @@ checkoutForm.addEventListener("submit", (event) => {
     console.log(event.target);
 });
 
+function displayShoppingItems(){
+    //Remove the previous cards
+    const orderItemsUl = document.querySelector(".order-items > ul");
+    orderItemsUl.innerHTML = "";
+    //Build each card
+    mockItemData.forEach(item => {
+        //Create elements
+        const itemCard = document.createElement("li");
+        const title = document.createElement("h4");
+        const description = document.createElement("h5");
+        const priceBtnDiv = document.createElement("div");
+        const price = document.createElement("p");
+        const addBtn = document.createElement("button");
+        //Modify elements
+        itemCard.classList.add("item")
+        title.textContent = `${item.title}`;
+        title.classList.add("shopping-item-title");
+        description.textContent = `${item.description}`;
+        description.classList.add("shopping-item-description");
+        priceBtnDiv.classList.add("shopping-item-priceBtnDiv");
+        price.textContent = `$${item.price}`;
+        price.classList.add("shopping-item-price");
+        addBtn.textContent = `Add to Cart`;
+        addBtn.classList.add("shopping-item-addToCart");
+        //Append elements
+        priceBtnDiv.append(price, addBtn);
+        itemCard.append(title, description, priceBtnDiv);
+        //Add the card to the screen
+        orderItemsUl.appendChild(itemCard);
+    })
+}
+
 const tallySheet = {};
 
 addToCartButtons.forEach((button) => {
@@ -124,3 +156,5 @@ function displayCart(){
     }
     //Add them to the DOM (aka display them)
 }
+
+displayShoppingItems();
